ba07889e5a3e5e2dab8c9540f8a8e6b7ebdd7519
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-06-09 23:11:12 +0545

    added change password page

--- lib/views/pages/change_password_page.dart
+++ lib/views/pages/change_password_page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-class UpdateProfilePage extends StatelessWidget {
-  const UpdateProfilePage({super.key});
+
+class ChangePasswordPage extends StatelessWidget {
+  const ChangePasswordPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: Text('Update Profile'),
+        title: Text('Change Password'),
         centerTitle: true,
       ),
     );
