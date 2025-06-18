7fdf76c27a8b3016f148e3872e6324e0be5108b5
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-06-18 23:29:39 +0545

    added error dialog

---
+++ lib/utils/errorDialog.dart
@@ -0,0 +1,36 @@
+import 'package:beenproject/main.dart';
+import 'package:flutter/material.dart';
+
+void MyDialog(
+    {BuildContext? context,
+      required String title,
+      required String message,
+      required String okText}) {
+  showDialog(
+      context: navigationKey.currentContext!,
+      builder: (context) {
+        return AlertDialog(
+          actions: [
+            InkWell(
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    okText,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ))
+          ],
+          title: Text(
+            title,
+            style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+          ),
+          content: Text(
+            message,
+            style: TextStyle(),
+          ),
+        );
+      });
+}
