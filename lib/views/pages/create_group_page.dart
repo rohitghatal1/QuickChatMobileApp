45d455e5544f4e2c15facf2ea71f5f0430bc87f0
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-06-29 18:55:05 +0545

    created new group page

---
+++ lib/views/pages/create_group_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class CreateGroupPage extends StatefulWidget {
+  const CreateGroupPage({super.key});
+
+  @override
+  State<CreateGroupPage> createState() => _CreateGroupPageState();
+}
+
+class _CreateGroupPageState extends State<CreateGroupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
